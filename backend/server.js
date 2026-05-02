b6df8bfea9fd898b47625dd5fed12bafd50e5e61
Swapnil <swwapnilrvte@gmail.com>
2026-05-03 00:58:27 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API Running");
+});
+
+app.listen(5000, () => {
+  console.log("Server started on port 5000");
+});
